07c88f3b0edb49c51b4ae60d180cb0ab5ea27cd2
AlxGr-OS <51755732+AlxGr-OS@users.noreply.github.com>
2019-09-20 13:33:58 -0400

    Update vardefs.php

--- modules/AOW_Conditions/vardefs.php
+++ modules/AOW_Conditions/vardefs.php
@@ -155,7 +155,7 @@ $dictionary['AOW_Condition'] = array(
     'reportable' => true,
     'unified_search' => false,
     'merge_filter' => 'disabled',
-    'len' => '255',
+    'len' => '1000',
     'size' => '20',
   ),
   'aow_workflow' =>
